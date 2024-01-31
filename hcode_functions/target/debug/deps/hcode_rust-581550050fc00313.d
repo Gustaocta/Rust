7911f7c71df4a64702b7700b6bedcb8694661d75
Gustavo-otacilio <gustavo.otacilio@grupotecnoset.com.br>
2024-01-31 11:58:15 -0300

    arquivo manifesto

---
+++ hcode_functions/target/debug/deps/hcode_rust-581550050fc00313.d
@@ -0,0 +1,5 @@
+C:\Users\gustavo.otacilio\Rust\hcode_functions\target\debug\deps\libhcode_rust-581550050fc00313.rmeta: src\lib.rs
+
+C:\Users\gustavo.otacilio\Rust\hcode_functions\target\debug\deps\hcode_rust-581550050fc00313.d: src\lib.rs
+
+src\lib.rs:
